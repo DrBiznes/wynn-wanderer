105a7f266217134f0abe091e55bcd4ef93029a42
Jam Femino <jamesfemino@gmail.com>
2025-04-18 15:15:36 -0700

    updating subtitles for cities

--- src/main/java/me/jamino/wynnWanderer/features/SignificantTerritoryManager.java
+++ src/main/java/me/jamino/wynnWanderer/features/SignificantTerritoryManager.java
@@ -5,10 +5,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SignificantTerritoryManager {
-    // List of significant cities/territories for special styling
+    // List of significant cities for special styling
     public static final Set<String> SIGNIFICANT_TERRITORIES = new HashSet<>(Arrays.asList(
             "Llevigar", "Gelibord", "Olux", "Rodoroc", "Eltom", "Cinfras", "Ahmsord",
             "Kandon-Beda", "Thesead", "Corkus City", "Selchar", "Nemract", "Almuj",
-            "Ragni", "Detlas", "Lutho", "Nesaak", "Troms"
+            "Ragni", "Detlas", "Lutho", "Nesaak", "Troms", "Alekin"
     ));
 }
